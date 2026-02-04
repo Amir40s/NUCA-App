59d3fe80b0de5e8d581ed611c01d4c13b33c6f98
Farhan Rehman <155618049+dashcode-007@users.noreply.github.com>
2026-02-04 12:08:17 +0500

    adding services

---
+++ lib/services/shared_preferences_service.dart
@@ -0,0 +1,53 @@
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  static const String _accessTokenKey = 'accessToken';
+  static const String _refreshTokenKey = 'refreshToken';
+  static const String _userIdKey = 'userId';
+  static const String _customerId = 'customerId';
+
+  static Future<void> saveLoginData(String accessToken,String refreshToken, String userId) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_accessTokenKey, accessToken);
+    await prefs.setString(_refreshTokenKey, _refreshTokenKey);
+    await prefs.setString(_userIdKey, userId );
+    // await prefs.setString(_userIdKey, userId);
+  }
+  static Future<void> saveTokens(String accessToken,String refreshToken,) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_accessTokenKey, accessToken);
+    await prefs.setString(_refreshTokenKey, _refreshTokenKey);
+  }
+
+  static Future<void> saveUserId(String userId) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_userIdKey, userId);
+  }
+
+  static Future<void> saveStripeCustomerId(String customerId) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_customerId, customerId);
+  }
+
+  static Future<String?> getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_accessTokenKey);
+  }
+  static Future<String?> getRefreshToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_refreshTokenKey);
+  }
+
+  static Future<String?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_userIdKey);
+  }
+
+  static Future<void> clearLoginData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_accessTokenKey);
+    await prefs.remove(_userIdKey);
+  }
+}
